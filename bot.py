5efb3601308881e04a790c07c42fe5cb7f78c5d1
hecronk <damiraminov1@yandex.ru>
2023-02-20 15:35:35 +0500

    fixes

--- bot.py
+++ bot.py
@@ -35,24 +35,28 @@ async def start(message: types.Message):
 @dp.message_handler(content_types='text')
 async def callback_handler(callback: types.CallbackQuery):
     if callback and user_has_permission(callback.from_user.id):
-        with open("DATA.txt", "r") as text_file:
-            loaded_str = text_file.read()
-        tech_time = datetime.datetime.strptime(loaded_str.split('TECHNICAL INFO \n')[1], '%d-%b-%Y (%H:%M:%S.%f)')
-        now = datetime.datetime.now()
-        delta = now - tech_time
-        warning_notification = delta >= datetime.timedelta(minutes=7)
-        if warning_notification:
-            await bot.send_message(
-                chat_id=callback.from_user.id,
-                text=f"WARNING: РАСПИСАНИЕ ПОСЛЕДНИЙ РАЗ ОБНОВЛЯЛОСЬ: {delta} ВРЕМЕНИ НАЗАД."
-                     f" АКТУАЛЬНОСТЬ РАСПИСАНИЯ ПОД ВОПРОСОМ, НАПИШИ ДАМИРУ ↓ https://t.me/damiraminov1"
-            )
-            await bot.send_message(
-                chat_id=callback.from_user.id,
-                text=f"А пока, посмотри пожалуйста, расписание самостоятельно:"
-                     f" https://uspu.ru/education/eios/schedule/?group_name=РиА-1931"
-            )
-        data = loaded_str.split('TECHNICAL INFO \n')[2].split('='*55)
-        for message in data:
-            if message:
-                await bot.send_message(chat_id=callback.from_user.id, text=message)
+        try:
+            with open("DATA.txt", "r") as text_file:
+                loaded_str = text_file.read()
+            tech_time = datetime.datetime.strptime(loaded_str.split('TECHNICAL INFO \n')[1], '%d-%b-%Y (%H:%M:%S.%f)')
+            now = datetime.datetime.now()
+            delta = now - tech_time
+            warning_notification = delta >= datetime.timedelta(minutes=7)
+            if warning_notification:
+                await bot.send_message(
+                    chat_id=callback.from_user.id,
+                    text=f"WARNING: РАСПИСАНИЕ ПОСЛЕДНИЙ РАЗ ОБНОВЛЯЛОСЬ: {delta} ВРЕМЕНИ НАЗАД."
+                         f" АКТУАЛЬНОСТЬ РАСПИСАНИЯ ПОД ВОПРОСОМ, НАПИШИ ДАМИРУ ↓ https://t.me/damiraminov1"
+                )
+                await bot.send_message(
+                    chat_id=callback.from_user.id,
+                    text=f"А пока, посмотри пожалуйста, расписание самостоятельно:"
+                         f" https://uspu.ru/education/eios/schedule/?group_name=РиА-1931"
+                )
+            data = loaded_str.split('TECHNICAL INFO \n')[2].split('=' * 55)
+            for message in data:
+                if message:
+                    await bot.send_message(chat_id=callback.from_user.id, text=message)
+        except BaseException:
+            await bot.send_message(chat_id=callback.from_user.id,
+                                   text="Подожди 10 минут, если не заработает, то пиши Дамиру ↓ https://t.me/damiraminov1")
